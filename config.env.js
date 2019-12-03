8d69b1a1417915116bc7d13a2476308b633020ef
Tony Coop <tony@thewholeworldwindow.co.uk>
2019-12-03 17:31:08 +0000

    Remove unused config key.

--- config.env.js
+++ config.env.js
@@ -1,7 +1,6 @@
 module.exports = {
   development: {
     isProduction: false,
-    assetsPath: `http://${process.env.HOST || 'localhost'}:${+process.env.PORT + 1 || 3000}/dist/`,
     host: process.env.HOST || 'localhost',
     port: +process.env.PORT || 3000
   }
